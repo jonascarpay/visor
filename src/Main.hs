4db8a61e72db48aa8126893a6579d206162b3e47
Jonas Carpay <jonascarpay@gmail.com>
2016-10-19 15:37:22 +0200

    Crop in terms of two coordinates, with (0,0) in the top left

--- src/Main.hs
+++ src/Main.hs
@@ -16,14 +16,14 @@ delete f = do exists <- doesFileExist f
               when exists $ removeFile f
 
 crop :: Int -> Int -> Int -> Int -> Array F DIM3 Word8 -> Array D DIM3 Word8
-crop x y w h img = cropped
-  where (Z:._:._:.d) = extent img
-        cropped = extract (Z:.y:.x:.d) (Z:.h:.w:.d) img
+crop x1 y1 x2 y2 img = cropped
+  where (Z:.h:._:.d) = extent img
+        cropped = extract (Z:.h-y2:.x1:.d) (Z:.y2-y1:.x2-x1:.d) img
 
 main :: IO ()
 main = do
   delete output
   runIL $ do
     (RGBA i) <- readImage input
-    cropped <- computeP $ crop 0 0 300 800 i :: IL (Array F DIM3 Word8)
+    cropped <- computeP $ crop 0 0 1272 1064 i :: IL (Array F DIM3 Word8)
     writeImage output (RGBA cropped)
