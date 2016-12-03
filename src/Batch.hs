fb3d730a5a0bae9dfcbc0d3100da6668e5d75427
Jonas Carpay <jonascarpay@gmail.com>
2016-12-03 17:46:42 +0100

    Input dimensionality is 3 times the input resolution

--- src/Batch.hs
+++ src/Batch.hs
@@ -43,17 +43,20 @@ imageToVector = fromList . (>>= unpackPixel) . V.toList . IC.vector
   where norm i = fromIntegral i / 255
         unpackPixel (RGBPixel r g b) = [norm r, norm g, norm b]
 
--- | Converts a list of r indices and an integer c to an
---   r x c matrix with 1 at horizontal position in the list
---   and 0 otherwise.
+-- | Converts a list of indices to a r x c matrix with 1's
+--   in the positions specified by the indices and 0 otherwise.
+--   This function is (-1)-indexed, as the first position is
+--   used for undefined outputs. This reserved position is useful
+--   to indicate that the feature does not occur, such as when a
+--   game is in a menu.
 --   toIndexMatrix' [3,2,1] 4 =
 --   0 0 0 1
 --   0 0 1 0
 --   0 1 0 0
 toIndexMatrix :: [Maybe Int] -> Int -> Matrix R
-toIndexMatrix y k = (length y><k) $ y >>= toIndex
-  where toIndex (Just y) = replicate y 0 ++ [1] ++ replicate (k-y-1) 0
-        toIndex Nothing  = replicate k 0
+toIndexMatrix y k = ( length y >< (k+1) ) $ y >>= toIndex
+  where toIndex (Just y) = 0 : (replicate y 0 ++ [1] ++ replicate (k-y-1) 0)
+        toIndex Nothing  = 1 : replicate k 0
 
 toIndexMatrix' :: [Int] -> Int -> Matrix R
 toIndexMatrix' = toIndexMatrix . fmap Just
