c65b50b843c86f60d6fa72724be1404e4846c5f4
Jonas Carpay <jonascarpay@gmail.com>
2016-12-14 21:30:04 +0100

    Repa GHC options in Volume module

--- src/Volume.hs
+++ src/Volume.hs
@@ -1,3 +1,7 @@
+{-# OPTIONS_GHC -Odph -rtsopts -threaded -fno-liberate-case
+                -funfolding-use-threshold1000 -funfolding-keeness-factor1000
+                -fllvm -optlo-O3 #-}
+
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE Strict #-}
 {-# LANGUAGE ScopedTypeVariables #-}
