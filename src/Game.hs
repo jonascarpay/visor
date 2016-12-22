86346879308f612e03504beb178407c8b625edb1
Jonas Carpay <jonascarpay@gmail.com>
2016-12-23 00:09:26 +0100

    Juicypixels and new spec-compatible loadImage fn

--- src/Game.hs
+++ src/Game.hs
@@ -6,6 +6,9 @@ module Game where
 import Util
 import Label
 import Codec.Picture
+import Codec.Picture.Extra
+import Conduit
+import System.Random
 
 -- | A Game defines where to get a certain data set,
 -- and what features to extract from it
@@ -62,5 +65,28 @@ data Dataset =
 
 -- | Loads an image and applies desired transformations
 loadFromSet :: Dataset
-            -> IOSrc Palette
-loadFromSet = undefined
+            -> IOSrc (Palette, [WidgetLabel])
+loadFromSet (Dataset root lblFn (Rect x y w h) wig dist) =
+  sourceDirectoryDeep True root .| loadImageC
+  where
+    loadImageC :: IOConduit FilePath (Palette, [WidgetLabel])
+    loadImageC = awaitForever$
+      \fp -> do o <- liftIO $ loadImage fp
+                yield o
+
+    loadImage :: FilePath -> IO (Palette, [WidgetLabel])
+    loadImage fp = do Right img' <- readImage fp
+                      dx <- randomRIO (0, wig)
+                      dy <- randomRIO (0, wig)
+                      dw <- randomRIO (0, wig)
+                      dh <- randomRIO (0, wig)
+                      dr :: Double <- randomRIO (0.9, 1.1)
+                      dg :: Double <- randomRIO (0.9, 1.1)
+                      db :: Double <- randomRIO (0.9, 1.1)
+                      let img = convertRGB8 img'
+                          imgCropped = crop (x+dx) (y+dy) (w-dx-dw) (h-dy-dh) img
+                          scaleMax x c = round $ min 255 $ fromIntegral x * c
+                          distortColor (PixelRGB8 r g b) = PixelRGB8 (scaleMax r dr) (scaleMax g dg) (scaleMax b db)
+                          distorted = pixelMap distortColor imgCropped
+
+                      return (if dist then distorted else imgCropped, lblFn fp)
