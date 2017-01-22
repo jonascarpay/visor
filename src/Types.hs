9a03a1184e509ffcea02241f0e12c9724b465d53
Jonas Carpay <jonascarpay@gmail.com>
2017-01-22 14:23:50 +0100

    Types module and deps

--- src/Types.hs
+++ src/Types.hs
@@ -1,8 +1,19 @@
-label -- Isomorf aan een one-hot vector, n lang
-widgetlabel -- Een vzl labels
-widgetseqnc -- Een vzl labels voor dezelfde widget
-imglabel -- een vzl widgetseqncs
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeOperators #-}
 
-plaatje -- Bevat meerdere widgetsqncs
-widgetsqnc -- extract uit een plaatje
-widget -- element uit een widgetsqnc
+module Types where
+
+import Data.Singletons.TypeLits
+import Data.Singletons.Prelude
+
+data Label (c :: Nat) = Label Int | NoParse
+
+data Widget (sh :: [Nat]) where
+  WNil  :: Widget '[]
+  WCons :: Label c -> Widget cs -> Widget (c ': cs)
+
+wcat :: Widget as -> Widget bs -> Widget (as :++ bs)
+wcat WNil           bs = bs
+wcat (a `WCons` as) bs = a `WCons` wcat as bs
