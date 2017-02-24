744456da7ee0eb03d14327354862dad35bf61ba5
Jonas Carpay <jonascarpay@gmail.com>
2017-02-24 21:31:37 +0100

    List dataset images with ls cmd

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,26 @@
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module Main where
 
 import Types
 import Games.Melee
 import System.Environment
+import Conduit
+import IO
 
 type Game = Melee
+
+set :: Dataset Melee
+set = dataset
 
 main :: IO ()
 main = getArgs >>= main'
 
 main' :: [String] -> IO ()
 main' ["parse", path] = print l
-  where l = parseFilename (dataset :: Dataset Game) path
+  where l = parseFilename set path
+
+main' ["ls"] =
+  do paths <- runConduitRes$ datasetPathSource set .| sinkList
+     putStrLn$ unlines paths
+     putStrLn$ show (length paths) ++ " images in dataset"
 
 main' l = putStrLn$ "Unrecognized argument list: " ++ unwords l
--- src/IO.hs
+++ src/IO.hs
@@ -1,12 +1,19 @@
 module IO
   ( readShot
+  , datasetPathSource
   ) where
 
 import Types
+import Lib
 import Static.Image
+import Conduit
+import System.FilePath
 
 readShot :: FilePath -> IO (Screenshot a)
 readShot fp = do ebmp <- readRaw fp
                  case ebmp of
                    Left err -> error err
                    Right bmp -> return (Screenshot bmp)
+
+datasetPathSource :: Dataset a -> RTSource FilePath
+datasetPathSource set = sourceDirectoryDeep True (rootDir set) .| filterC ((== ".bmp") . takeExtension)
