c5ac5c09b5bf44e542d24eab911e3d9843e3274c
Jonas Carpay <jonascarpay@gmail.com>
2017-04-08 01:39:52 +0200

    Rewrite Melee conduit logic with new buffer

--- src/Games/Melee/Conduits.hs
+++ src/Games/Melee/Conduits.hs
@@ -1,7 +1,3 @@
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module Games.Melee.Conduits where
 
 import Conduit
@@ -12,42 +8,31 @@ import Codec.Picture( PixelRGBA8( .. ), writePng )
 import Graphics.Rasterific
 import Graphics.Rasterific.Texture
 import Data.Singletons.TypeLits
-import Data.Proxy
 
 type MeleeB s = Buffer s Melee
 type MeleeGame = [Melee]
 
-onGameEnd :: forall s. KnownNat s => (MeleeGame -> IO ()) -> RTConduit (MeleeB s) (MeleeB s)
+onGameEnd :: KnownNat s => (MeleeGame -> IO ()) -> RTConduit (MeleeB s) (MeleeB s)
 onGameEnd handler = go False
   where
-
-    bufsize = fromIntegral$ natVal (Proxy :: Proxy s)
-    isStart buf = Menu `notElem` buf && length buf == bufsize && isStartFrame (last buf)
-
-    isEnd buf = all (==Menu) (init buf) && length buf == bufsize && isEndFrame (last buf)
-
-    go False = do mbuf <- await
-                  case mbuf of
-                    Nothing -> return ()
-                    Just (Buffer (take bufsize -> buf)) ->
-                      if isStart buf
-                       then liftIO (putStrLn "Starting game") >> go True
-                       else liftIO (putStr "F") >> go False
-
-    go True = do mbuf <- await
-                 case mbuf of
-                   Nothing -> return ()
-                   Just b@(Buffer buf) ->
-                     if isEnd (take bufsize buf)
-                        then do liftIO$ putStrLn "Game over"
-                                liftIO$ handler (extractGame b)
-                                go False
-
-                        else go True
+    go ingame = do mbuf <- await
+                   case (ingame, mbuf) of
+                      (_, Nothing) -> return ()
+                      (False, Just buf) ->
+                        if maybe False isStartFrame (bufHead buf)
+                         then liftIO (putStrLn "Starting game") >> go True
+                         else liftIO (putStr "F") >> go False
+                      (True, Just buf) ->
+                        if maybe True isEndFrame (bufHead buf)
+                         then do liftIO$ putStrLn "Game over"
+                                 liftIO$ handler (extractGame buf)
+                                 go False
+                         else liftIO (putStr "T") >> go False
 
 extractGame :: MeleeB s -> MeleeGame
 extractGame = undefined
 
+isStartFrame, isEndFrame :: Melee -> Bool
 isStartFrame (Ingame (PlayerState 4 0) _ (PlayerState 4 0) _ ) = True
 isStartFrame _ = False
 
